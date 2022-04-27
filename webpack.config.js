306332a98a58fa6a19b354785bb92f37fe3d8439
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-04-27 22:06:43 +0400

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  devtoo: 'source-map',
+};
