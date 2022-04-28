ad8f173211cca25cb12345138030a7d26965cc5d
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-04-28 14:24:59 +0400

    Add 'extra-content' to 'content-container'

--- src/presenter/content-presenter.js
+++ src/presenter/content-presenter.js
@@ -4,23 +4,26 @@ import MainContentView from '../view/main-content-view.js';
 import ListContainerView from '../view/list-container-view.js';
 import CardView from '../view/card-view.js';
 import ShowMoreButtonView from '../view/show-more-button-view.js';
+import ExtraContentView from '../view/extra-content-view.js';
 
 export default class ContentPresenter {
   contentContainerComponent = new ContentContainerView();
   mainContentComponent = new MainContentView();
+  extraContentComponent = new ExtraContentView();
   listContainerComponent = new ListContainerView();
 
   init(board) {
     this.board = board;
 
     render(this.contentContainerComponent, this.board);
+
     render(this.mainContentComponent, this.contentContainerComponent.getElement());
     render(this.listContainerComponent, this.mainContentComponent.getElement());
-
     for (let i = 0; i < 5; i++) {
       render(new CardView(), this.listContainerComponent.getElement());
     }
-
     render(new ShowMoreButtonView(), this.mainContentComponent.getElement());
+
+    render(this.extraContentComponent, this.contentContainerComponent.getElement());
   }
 }
