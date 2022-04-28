ccb9ebf81180113d70efcde206890515782f060b
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-04-28 13:43:12 +0400

    Add 'list-container' to 'main-content'

--- src/presenter/content-presenter.js
+++ src/presenter/content-presenter.js
@@ -1,14 +1,18 @@
 import { render } from '../render.js';
 import ContentContainerView from '../view/content-container-view.js';
 import MainContentView from '../view/main-content-view.js';
+import ListContainerView from '../view/list-container-view.js';
 
 export default class ContentPresenter {
   contentContainerComponent = new ContentContainerView();
+  mainContentComponent = new MainContentView();
+  listContainerComponent = new ListContainerView();
 
   init(board) {
     this.board = board;
 
     render(this.contentContainerComponent, this.board);
-    render(new MainContentView(), this.contentContainerComponent.getElement());
+    render(this.mainContentComponent, this.contentContainerComponent.getElement());
+    render(this.listContainerComponent, this.mainContentComponent.getElement());
   }
 }
