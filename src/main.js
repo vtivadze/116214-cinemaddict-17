9357136295d467fe79622f698931cb63cc5da223
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-04-28 15:05:27 +0400

    Add 'films-count' to 'footer-statistics'

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import {render} from './render.js';
 import UserTitleView from './view/user-title-view.js';
 import MenuView from './view/menu-view.js';
 import SortView from './view/sort-view.js';
-// import ContentContainerView from './view/content-container-view.js';
+import FilmsCountView from './view/films-count-view.js';
 import ContentPresenter from './presenter/content-presenter.js';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
+const siteFooterElement = document.querySelector('.footer');
+const footerStatisticsElement = siteFooterElement.querySelector('.footer__statistics');
+
 const contentPresenter = new ContentPresenter();
 
 render(new UserTitleView(), siteHeaderElement);
@@ -14,3 +17,5 @@ render(new MenuView(), siteMainElement);
 render(new SortView(), siteMainElement);
 
 contentPresenter.init(siteMainElement);
+
+render(new FilmsCountView(), footerStatisticsElement);
