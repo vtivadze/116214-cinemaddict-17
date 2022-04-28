030fe786c04804970d38e53ad1a02ecd3602f00f
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-04-28 10:39:10 +0400

    Add filter into site main element

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import {render} from './render.js';
 import UserTitleView from './view/user-title-view.js';
+import FilterView from './view/filter-view.js';
 
 const siteHeaderElement = document.querySelector('.header');
+const siteMainElement = document.querySelector('.main');
 
 render(new UserTitleView(), siteHeaderElement);
-
+render(new FilterView(), siteMainElement);
