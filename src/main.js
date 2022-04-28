eb833b105fea542e2db96f21890f987ddf3682ec
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-04-28 11:50:53 +0400

    Add 'content-container' to site main element

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {render} from './render.js';
 import UserTitleView from './view/user-title-view.js';
 import MenuView from './view/menu-view.js';
 import SortView from './view/sort-view.js';
+import ContentContainerView from './view/content-container-view.js';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
@@ -9,3 +10,4 @@ const siteMainElement = document.querySelector('.main');
 render(new UserTitleView(), siteHeaderElement);
 render(new MenuView(), siteMainElement);
 render(new SortView(), siteMainElement);
+render(new ContentContainerView(), siteMainElement);
