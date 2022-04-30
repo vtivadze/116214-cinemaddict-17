a66daa9f3214951865a33d41e110f37aad15c287
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-04-30 14:52:48 +0400

    Create a function 'generateDescription'

--- src/mock/movie.js
+++ src/mock/movie.js
@@ -108,6 +108,13 @@ const generateGenre = () => {
   return genres[getRandomInteger(0, genres.length - 1)];
 };
 
+const generateDescription = () => {
+  const descriptions = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.';
+  const descriptionsArray = descriptions.split('. ');
+
+  return descriptionsArray[getRandomInteger(0, descriptionsArray.length - 1)];
+};
+
 export const generateMovie = () => ({
   id: generateId(),
   comments: [5, 6],
@@ -135,7 +142,7 @@ export const generateMovie = () => ({
       generateGenre(),
       generateGenre()
     ],
-    description: 'Oscar-winning film, a war drama about two young people, from the creators of timeless classic "Nu, Pogodi!" and "Alice in Wonderland", with the best fight scenes since Bruce Lee.'
+    description: generateDescription()
   },
   userDetails: {
     watchlist: false,
