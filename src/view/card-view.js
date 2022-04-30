59cc3175c309008e58462094c78cd25d4afa5ac9
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-04-30 21:08:08 +0400

    Display 'realease date' in 'card-view'

--- src/view/card-view.js
+++ src/view/card-view.js
@@ -1,7 +1,25 @@
 import { createElement } from '../render.js';
+import { humanizeDate } from '../util.js';
 
 const createCardTemplate = (movie) => {
-  const {filmInfo: {title, totalRating, runtime, genre, poster, description}, comments} = movie;
+  const {
+    filmInfo: {
+      title,
+      totalRating,
+      runtime,
+      genre,
+      poster,
+      description,
+      release: {
+        date
+      }
+    },
+    comments
+  } = movie;
+
+  const releaseDate = humanizeDate(date);
+  const genres = genre.join(', ');
+  const commentsCount = comments.length;
 
   return  (
     `<article class="film-card">
@@ -9,13 +27,13 @@ const createCardTemplate = (movie) => {
         <h3 class="film-card__title">${title}</h3>
         <p class="film-card__rating">${totalRating}</p>
         <p class="film-card__info">
-          <span class="film-card__year">1936</span>
+          <span class="film-card__year">${releaseDate}</span>
           <span class="film-card__duration">${runtime}m</span>
-          <span class="film-card__genre">${genre.join(', ')}</span>
+          <span class="film-card__genre">${genres}</span>
         </p>
         <img src="./images/posters/${poster}" alt="" class="film-card__poster">
         <p class="film-card__description">${description}</p>
-        <span class="film-card__comments">${comments.length} comments</span>
+        <span class="film-card__comments">${commentsCount} comments</span>
       </a>
       <div class="film-card__controls">
         <button class="film-card__controls-item film-card__controls-item--add-to-watchlist film-card__controls-item--active" type="button">Add to watchlist</button>
