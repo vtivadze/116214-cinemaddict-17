8358a34b1e56e1fd0f68c478ce47c4bc36d799fb
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-04-30 20:18:10 +0400

    Refactor 'card-view.js'

--- src/view/card-view.js
+++ src/view/card-view.js
@@ -1,26 +1,31 @@
 import { createElement } from '../render.js';
 
-const createCardTemplate = ({filmInfo: {title, totalRating, runtime, genre, poster, description}, comments}) => (
-  `<article class="film-card">
-    <a class="film-card__link">
-      <h3 class="film-card__title">${title}</h3>
-      <p class="film-card__rating">${totalRating}</p>
-      <p class="film-card__info">
-        <span class="film-card__year">1936</span>
-        <span class="film-card__duration">${runtime}m</span>
-        <span class="film-card__genre">${genre.join(', ')}</span>
-      </p>
-      <img src="./images/posters/${poster}" alt="" class="film-card__poster">
-      <p class="film-card__description">${description}</p>
-      <span class="film-card__comments">${comments.length} comments</span>
-    </a>
-    <div class="film-card__controls">
-      <button class="film-card__controls-item film-card__controls-item--add-to-watchlist film-card__controls-item--active" type="button">Add to watchlist</button>
-      <button class="film-card__controls-item film-card__controls-item--mark-as-watched film-card__controls-item--active" type="button">Mark as watched</button>
-      <button class="film-card__controls-item film-card__controls-item--favorite film-card__controls-item--active" type="button">Mark as favorite</button>
-    </div>
-  </article>`
-);
+const createCardTemplate = (movie) => {
+  const {filmInfo: {title, totalRating, runtime, genre, poster, description}, comments} = movie;
+
+  return  (
+    `<article class="film-card">
+      <a class="film-card__link">
+        <h3 class="film-card__title">${title}</h3>
+        <p class="film-card__rating">${totalRating}</p>
+        <p class="film-card__info">
+          <span class="film-card__year">1936</span>
+          <span class="film-card__duration">${runtime}m</span>
+          <span class="film-card__genre">${genre.join(', ')}</span>
+        </p>
+        <img src="./images/posters/${poster}" alt="" class="film-card__poster">
+        <p class="film-card__description">${description}</p>
+        <span class="film-card__comments">${comments.length} comments</span>
+      </a>
+      <div class="film-card__controls">
+        <button class="film-card__controls-item film-card__controls-item--add-to-watchlist film-card__controls-item--active" type="button">Add to watchlist</button>
+        <button class="film-card__controls-item film-card__controls-item--mark-as-watched film-card__controls-item--active" type="button">Mark as watched</button>
+        <button class="film-card__controls-item film-card__controls-item--favorite film-card__controls-item--active" type="button">Mark as favorite</button>
+      </div>
+    </article>`
+  );
+}
+;
 
 export default class CardView {
   constructor(movie) {
