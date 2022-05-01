7a7a9eca4332d303b68f87323b06a74244f54ae7
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-05-01 21:21:08 +0400

    Change comments count in 'comments-model'

--- src/model/comments-model.js
+++ src/model/comments-model.js
@@ -1,6 +1,6 @@
 import { generateComment } from '../mock/comment.js';
 
-const COMMENTS_COUNT = 4;
+const COMMENTS_COUNT = 5;
 
 export default class CommentsModel {
   comments = Array.from({length: COMMENTS_COUNT}, generateComment);
