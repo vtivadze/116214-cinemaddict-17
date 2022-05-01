a3bc161034793203288b4095fe65f98ecf3aa083
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-05-01 16:54:15 +0400

    Create 'comment-view'

---
+++ src/view/comment-view.js
@@ -0,0 +1,35 @@
+import {createElement} from '../render.js';
+
+const createCommentTemplate = () => (
+  `<li class="film-details__comment">
+    <span class="film-details__comment-emoji">
+      <img src="./images/emoji/smile.png" width="55" height="55" alt="emoji-smile">
+    </span>
+    <div>
+      <p class="film-details__comment-text">Interesting setting and a good cast</p>
+      <p class="film-details__comment-info">
+        <span class="film-details__comment-author">Tim Macoveev</span>
+        <span class="film-details__comment-day">2019/12/31 23:59</span>
+        <button class="film-details__comment-delete">Delete</button>
+      </p>
+    </div>
+  </li>`
+);
+
+export default class CommentView {
+  getTemplate() {
+    return createCommentTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
