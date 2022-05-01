43f7e6dc29ef94a43ac55283cb531d76961c7561
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-05-01 14:56:50 +0400

    Apply function 'getId' for generating movies' 'id' in 'src/mock/movie.js'

--- src/mock/movie.js
+++ src/mock/movie.js
@@ -1,10 +1,10 @@
 import {
   getRandomInteger,
   getRandomFloat,
-  getArrayRandomElement
+  getArrayRandomElement,
+  getId
 } from '../util.js';
 
-const MOVIES_COUNT = 7;
 const MAX_TOTAL_RAITING = 10;
 const MAX_AGE_RAITING = 100;
 const MAX_COMMENT_ID = 100;
@@ -85,7 +85,7 @@ const GENRES = [
 
 const DESCRIPTIONS = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.';
 
-const generateId = () => getRandomInteger(0, MOVIES_COUNT).toString();
+const generateId = getId();
 const generateTitle = () => getArrayRandomElement(TITLES);
 const generateTotalRating = () => getRandomFloat(0, MAX_TOTAL_RAITING);
 const generateAgeRating = () => getRandomInteger(0, MAX_AGE_RAITING);
