a6e19207aa3826934a8af1fad41ddc72838f5f1d
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-05-01 15:43:32 +0400

    Create a function 'getMockText' in 'util.js'

--- src/util.js
+++ src/util.js
@@ -2,6 +2,8 @@ import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
+const TEXT_EXAMPLE = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.';
+
 const getRandomInteger = (min = 0, max = 1) => {
   min = Math.ceil(Math.min(min, max));
   max = Math.floor(Math.max(min, max));
@@ -35,6 +37,17 @@ const getId = () => {
   return () => (id++).toString();
 };
 
+const getMockText = (MAX_SENTENCE_COUNT) => {
+  const textArray = TEXT_EXAMPLE.split('. ');
+  const sentenceCount = getRandomInteger(1, MAX_SENTENCE_COUNT);
+  let mockText = '';
+  for (let i = 0; i < sentenceCount; i++) {
+    mockText += getArrayRandomElement(textArray);
+    mockText += '. ';
+  }
+  return mockText;
+};
+
 export {
   getRandomInteger,
   getRandomFloat,
@@ -42,5 +55,6 @@ export {
   humanizeDate,
   humanizeYear,
   humanizeDuration,
-  getId
+  getId,
+  getMockText
 };
