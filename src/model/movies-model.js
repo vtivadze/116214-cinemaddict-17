5d17bbb6e6ab4ae2eece8df0bf2623c0f4f45700
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-05-02 19:06:25 +0400

    Refactor functions 'getWatchListCount', 'getAlreadyWatchedCount' and 'getFavoreiteCount'

--- src/model/movies-model.js
+++ src/model/movies-model.js
@@ -6,7 +6,7 @@ export default class MoviesModel {
   movies = Array.from({length: MOVIES_COUNT}, generateMovie);
 
   getMovies = () => this.movies;
-  getWatchListCount = () => this.movies.reduce((count, movie) => movie.userDetails.watchlist ? ++count : count, 0);
-  getAlreadyWatchedCount = () => this.movies.reduce((count, movie) => movie.userDetails.alreadyWatched ? ++count : count, 0);
-  getFavoreiteCount = () => this.movies.reduce((count, movie) => movie.userDetails.favorite ? ++count : count, 0);
+  getWatchListCount = () => this.movies.filter((movie) => movie.userDetails.watchlist).length;
+  getAlreadyWatchedCount = () => this.movies.filter((movie) => movie.userDetails.alreadyWatched).length;
+  getFavoreiteCount = () => this.movies.filter((movie) => movie.userDetails.favorite).length;
 }
