454a90b792ccccbc1a1592e6f47963f9849cafb3
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-05-03 17:12:34 +0400

    Apply class new features in 'comments-model'

--- src/model/comments-model.js
+++ src/model/comments-model.js
@@ -3,7 +3,9 @@ import { generateComment } from '../mock/comment.js';
 const COMMENTS_COUNT = 5;
 
 export default class CommentsModel {
-  comments = Array.from({length: COMMENTS_COUNT}, generateComment);
+  #comments = Array.from({length: COMMENTS_COUNT}, generateComment);
 
-  getComments = () => this.comments;
+  get comments () {
+    return this.#comments;
+  }
 }
