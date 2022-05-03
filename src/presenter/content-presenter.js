9f354ef72fca127fdf090c1ad7b62d74eedd163d
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-05-03 14:34:53 +0400

    Introduce a private method 'renderMovie' in the class 'ContentPresenter'

--- src/presenter/content-presenter.js
+++ src/presenter/content-presenter.js
@@ -8,7 +8,6 @@ import TopRatedView from '../view/top-rated-view.js';
 import MostCommented from '../view/most-commented-view.js';
 
 export default class ContentPresenter {
-  CARDS_COUNT = 5;
   CARDS_COUNT_EXTRA = 2;
 
   #board = null;
@@ -35,21 +34,27 @@ export default class ContentPresenter {
 
     render(this.#mainContentComponent, this.#contentContainerComponent.element);
     render(this.#mainContentListComponent, this.#mainContentComponent.element);
-    for (let i = 0; i < this.CARDS_COUNT; i++) {
-      render(new CardView(this.#movies[i]), this.#mainContentListComponent.element);
+    for (let i = 0; i < this.#movies.length; i++) {
+      this.#renderMovie(this.#movies[i], this.#mainContentListComponent);
     }
     render(new ShowMoreButtonView(), this.#mainContentComponent.element);
 
     render(this.#extraContentComponent, this.#contentContainerComponent.element);
     render(this.#extraContentListComponent, this.#extraContentComponent.element);
     for (let i = 0; i < this.CARDS_COUNT_EXTRA; i++) {
-      render(new CardView(this.#movies[i]), this.#extraContentListComponent.element);
+      this.#renderMovie(this.#movies[i], this.#extraContentListComponent);
     }
 
     render(this.#mostCommentedComponent, this.#contentContainerComponent.element);
     render(this.#mostCommentedListcomponent, this.#mostCommentedComponent.element);
     for (let i = 0; i < this.CARDS_COUNT_EXTRA; i++) {
-      render(new CardView(this.#movies[i]), this.#mostCommentedListcomponent.element);
+      this.#renderMovie(this.#movies[i], this.#mostCommentedListcomponent);
     }
+  }
+
+  #renderMovie(movie, containerComponent) {
+    const cardComponent = new CardView(movie);
+    const container = containerComponent.element;
+    render(cardComponent, container);
   }
 }
