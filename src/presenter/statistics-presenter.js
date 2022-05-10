41afdd1019f64c32735cc521ab7394157bfabc6d
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-05-10 18:43:32 +0400

    Create 'statistics-presenter'

---
+++ src/presenter/statistics-presenter.js
@@ -0,0 +1,22 @@
+import {render} from '../render.js';
+import StatisticsView from '../view/statistics-view.js';
+
+export default class StatisticsPresenter {
+  #container = null;
+  #moviesModel = null;
+  #moviecCount = 0;
+
+  constructor(container, moviesModel) {
+    this.#container = container;
+    this.#moviesModel = moviesModel;
+  }
+
+  init() {
+    this.#moviecCount = this.#moviesModel.movies.length;
+    return this;
+  }
+
+  renderStatistics() {
+    render(new StatisticsView(this.#moviecCount), this.#container);
+  }
+}
