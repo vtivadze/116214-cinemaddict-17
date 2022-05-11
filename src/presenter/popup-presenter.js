15525ffadd920c23a35381c07613ebcfbb8a9dbe
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-05-11 22:23:36 +0400

    Rename event handler method 'handleEscapeDocument' to 'onEscapeDocument'

--- src/presenter/popup-presenter.js
+++ src/presenter/popup-presenter.js
@@ -14,7 +14,7 @@ export default class PopupPresenter {
 
   init() {
     this.#popupComponent = new PopupView(this.#movie, this.#comments);
-    document.addEventListener('keydown', this.#handleEscapeDocument);
+    document.addEventListener('keydown', this.#onEscapeDocument);
     this.#popupComponent.setCloseButtonClickHandler(this.#hidePopup);
     return this;
   }
@@ -24,7 +24,7 @@ export default class PopupPresenter {
     render(this.#popupComponent, document.body);
   }
 
-  #handleEscapeDocument = (evt) => {
+  #onEscapeDocument = (evt) => {
     if (isEscape(evt.code)) {
       evt.preventDefault();
       this.#hidePopup();
@@ -34,6 +34,6 @@ export default class PopupPresenter {
   #hidePopup = () => {
     this.#popupComponent.element.remove();
     document.body.classList.remove('hide-overflow');
-    document.removeEventListener('keydown', this.#handleEscapeDocument);
+    document.removeEventListener('keydown', this.#onEscapeDocument);
   };
 }
