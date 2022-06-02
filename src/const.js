b44db35364961421e7b24019aa288201ba9c6298
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-06-02 14:53:47 +0400

    Create constant 'SortType' in 'const.js'

--- src/const.js
+++ src/const.js
@@ -2,4 +2,15 @@ const MOVIE_COUNT_PER_STEP = 5;
 const MOST_COMMETNTED_COUNT = 2;
 const TOP_RATED_COUNT = 2;
 
-export {MOVIE_COUNT_PER_STEP, MOST_COMMETNTED_COUNT, TOP_RATED_COUNT};
+const SortType = {
+  DEFAULT: 'default',
+  DATE: 'date',
+  RATING: 'rating'
+};
+
+export {
+  MOVIE_COUNT_PER_STEP,
+  MOST_COMMETNTED_COUNT,
+  TOP_RATED_COUNT,
+  SortType
+};
