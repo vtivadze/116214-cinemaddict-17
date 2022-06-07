221f54489a803f99f242f0cf201871e907b7b210
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-06-07 12:21:15 +0400

    Create a method 'updateMovie' in 'MoviesMdel'

--- src/const.js
+++ src/const.js
@@ -11,4 +11,29 @@ const emojies = [
   'angry',
 ];
 
-export {SortType, emojies};
+const UserAction = {
+  CARD_CONTROLS: 'CARD_CONTROLS',
+  SORT: 'SORT',
+  FILTER: 'FILTER',
+  ADD_COMMENT: 'ADD_COMMENT',
+  DELETE_COMMENT: 'DELETE_COMMENT',
+};
+
+const UpdateType = {
+  // click on buttons: toWatchelist, favorites
+  FILTER: ['cards', 'filter'],
+
+  // click on button: alreadyWathced
+  PROFILE: ['cards', 'filter', 'profile'],
+
+  // add, remove comment
+  COMMENT: ['cards', 'most_commented'],
+
+  // sort, filter
+  BOARD: ['main_container'],
+
+  // click on buttons: toWatchlist, favorites, alreadyWatched
+  ACTIVE_FILTER: ['cards', 'filter', 'profile', 'main_container']
+};
+// 51 min
+export {SortType, emojies, UserAction, UpdateType};
--- src/model/movies-model.js
+++ src/model/movies-model.js
@@ -10,6 +10,22 @@ export default class MoviesModel extends Observable {
     return this.#movies;
   }
 
+  updateMovie = (updateType, update) => {
+    const index = this.#movies.findIndex((movie) => movie.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t update unexiting movie');
+    }
+
+    this.#movies = [
+      ...this.#movies.slice(0, index),
+      update,
+      ...this.#movies.slice(index + 1)
+    ];
+
+    this._notify(updateType, update);
+  };
+
   get watchListCount() {
     return this.#movies.filter((movie) => movie.userDetails.watchlist).length;
   }
