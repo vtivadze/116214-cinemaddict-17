88e9817bec36cde3322615ec0acf2488f50eadd9
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-06-11 17:03:26 +0400

    Inherit CommentsModel from Observable

--- src/model/comments-model.js
+++ src/model/comments-model.js
@@ -1,11 +1,28 @@
+import Observable from '../framework/observable.js';
 import { generateComment } from '../mock/comment.js';
 
 const COMMENTS_COUNT = 20;
 
-export default class CommentsModel {
+export default class CommentsModel extends Observable {
   #comments = Array.from({length: COMMENTS_COUNT}, generateComment);
 
   get comments () {
     return this.#comments;
   }
+
+  updateComment = (updateType, update) => {
+    const index = this.#comments.findIndex((comment) => comment.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t update unexiting comment');
+    }
+
+    this.#comments = [
+      ...this.#comments.slice(0, index),
+      update,
+      ...this.#comments.slice(index + 1)
+    ];
+
+    this._notify(updateType, update);
+  };
 }
