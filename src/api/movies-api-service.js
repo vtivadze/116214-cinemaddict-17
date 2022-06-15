adac9823473427e62d41f9b9846ded95086b1626
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-06-15 18:13:07 +0400

    Create MovieApiService for making REST requests to movies' resourses

---
+++ src/api/movies-api-service.js
@@ -0,0 +1,26 @@
+import ApiService from '../framework/api-service.js';
+
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+export default class MoviesApiService extends ApiService {
+  get movies() {
+    return this._load({url: 'movies'})
+      .then(ApiService.parseResponse);
+  }
+
+  updateMovie = async (movie) => {
+    const response = await this._load({
+      url: `movies/${movie.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(movie),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+
+    const parsedResponse = await ApiService.parseResponse(response);
+
+    return parsedResponse;
+  };
+}
