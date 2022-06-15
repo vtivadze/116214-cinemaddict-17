d5f214c8d62cb7dfeb179370125ef6cd5b8ff4ba
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-06-15 20:47:32 +0400

    Create adapter to adapt client movie data for server movie data

--- src/api/movies-api-service.js
+++ src/api/movies-api-service.js
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import ApiService from '../framework/api-service.js';
 
 const Method = {
@@ -15,12 +16,42 @@ export default class MoviesApiService extends ApiService {
     const response = await this._load({
       url: `movies/${movie.id}`,
       method: Method.PUT,
-      body: JSON.stringify(movie),
+      body: JSON.stringify(this.#adaptToServer(movie)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  };
+
+  #adaptToServer = (movie) => {
+    const adaptedMovie = {...movie,
+      'film_info': {...movie.filmInfo,
+        'age_rating': movie.filmInfo.ageRating,
+        'alternative_title': movie.filmInfo.alternativeTitle,
+        'total_rating': movie.filmInfo.totalRating,
+        'release': {...movie.filmInfo.release,
+          'release_country': movie.filmInfo.release.releaseCountry,
+        },
+      },
+      'user_etails': {...movie.userDetails,
+        'already_watched': movie.userDetails.alreadyWatched,
+        'watching_date': dayjs(movie.userDetails.watchingDate).toISOString(),
+      },
+    };
+
+    delete(adaptedMovie.filmInfo);
+    delete(adaptedMovie.userDetails);
+
+    delete(adaptedMovie.film_info.ageRating);
+    delete(adaptedMovie.film_info.alternativeTitle);
+    delete(adaptedMovie.film_info.totalRating);
+    delete(adaptedMovie.film_info.release.releaseCountry);
+
+    delete(adaptedMovie.user_details.alreadyWatched);
+    delete(adaptedMovie.user_details.watchingDate);
+
+    return adaptedMovie;
   };
 }
